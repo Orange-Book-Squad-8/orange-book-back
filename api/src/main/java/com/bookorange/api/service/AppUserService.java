27dc171a5a6ef4cb63312b3750f9e1743477cf88
MarceloRibeiro1 <86325217+MarceloRibeiro1@users.noreply.github.com>
2022-11-07 15:11:54 -0300

    feat: new methods

--- api/src/main/java/com/bookorange/api/service/AppUserService.java
+++ api/src/main/java/com/bookorange/api/service/AppUserService.java
@@ -2,6 +2,7 @@ package com.bookorange.api.service;
 
 import com.bookorange.api.domain.AppUser;
 import com.bookorange.api.domain.Role;
+import com.bookorange.api.dto.appuserDto.AppUserCourseEditDTO;
 import com.bookorange.api.dto.appuserDto.AppUserDTO;
 import com.bookorange.api.dto.appuserDto.UserCreateDTO;
 
@@ -19,4 +20,18 @@ public interface AppUserService {
     List<AppUser> findAll();
 
     void remove(Long userId);
+
+    void addSubscribedCourse(AppUserCourseEditDTO userDto);
+
+    void removeSubscribedCourse(AppUserCourseEditDTO userDto);
+
+    void addArchivedCourse(AppUserCourseEditDTO userDto);
+
+    void removeArchivedCourse(AppUserCourseEditDTO userDto);
+
+    void addMyCourse(AppUserCourseEditDTO userDto);
+
+    void removeMyCourse(AppUserCourseEditDTO userDto);
+
+    void finishCourse(AppUserCourseEditDTO userDto);
 }
