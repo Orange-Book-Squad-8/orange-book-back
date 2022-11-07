a48f4e6d569bc2256e9fd95e616a40bf0fe535aa
Caio André <caioserralvo182@gmail.com>
2022-11-07 19:10:01 -0300

    feat: RoleController

--- api/src/main/java/com/bookorange/api/controller/RoleController.java
+++ api/src/main/java/com/bookorange/api/controller/RoleController.java
@@ -1,4 +1,33 @@
 package com.bookorange.api.controller;
 
+import com.bookorange.api.domain.AppUser;
+import com.bookorange.api.domain.Role;
+import com.bookorange.api.dto.appuserDto.AppUserDTO;
+import com.bookorange.api.dto.appuserDto.UserCreateDTO;
+import com.bookorange.api.dto.roleDto.RoleCreateDTO;
+import com.bookorange.api.dto.roleDto.RoleDTO;
+import com.bookorange.api.service.implementation.RoleServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/roles")
 public class RoleController {
+
+    @Autowired
+    private RoleServiceImp roleServiceImp;
+
+    @PostMapping(value = "/create")
+    public ResponseEntity<RoleDTO> createRole(@RequestBody RoleCreateDTO roleCreateDTO) {
+        try {
+            Role createdRole = roleServiceImp.create(roleCreateDTO);
+            return ResponseEntity.ok(new RoleDTO(createdRole));
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
