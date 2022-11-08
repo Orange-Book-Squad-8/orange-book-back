2dc41921ccbd99c1886653e677998951df682a6a
Caio André <caioserralvo182@gmail.com>
2022-11-08 19:12:57 -0300

    chore: AppUserController / CourseController

--- api/src/main/java/com/bookorange/api/controller/AppUserController.java
+++ api/src/main/java/com/bookorange/api/controller/AppUserController.java
@@ -1,14 +1,14 @@
 package com.bookorange.api.controller;
 
+
 import com.bookorange.api.domain.AppUser;
 import com.bookorange.api.domain.Role;
 import com.bookorange.api.dto.appuserDto.*;
 import com.bookorange.api.dto.watchedDto.WatchedLessonDTO;
-import com.bookorange.api.service.implementation.AppUserServiceImp;
-import com.bookorange.api.service.implementation.RoleServiceImp;
-import com.bookorange.api.service.implementation.WatchedListServiceImp;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.bookorange.api.service.AppUserService;
+import com.bookorange.api.service.RoleService;
+import com.bookorange.api.service.WatchedListService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,33 +17,31 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/users")
+@AllArgsConstructor
 public class AppUserController {
 
-    @Autowired
-    private AppUserServiceImp appUserServiceImp;
+    private final AppUserService appUserService;
 
-    @Autowired
-    private RoleServiceImp roleServiceImp;
+    private final RoleService roleService;
 
-    @Autowired
-    private WatchedListServiceImp watchedListServiceImp;
+    private final WatchedListService watchedListService;
 
 
     @PostMapping(value = "/create")
     public ResponseEntity<AppUserDTO> createAppUser(@RequestBody UserCreateDTO userCreateDTO) {
         try {
-            Role role = roleServiceImp.findByName(userCreateDTO.getRole());
-            AppUser createdAppUser = appUserServiceImp.create(userCreateDTO, role);
+            Role role = roleService.findByName(userCreateDTO.getRole());
+            AppUser createdAppUser = appUserService.create(userCreateDTO, role);
             return ResponseEntity.ok(new AppUserDTO(createdAppUser));
         } catch (RuntimeException e) {
             throw new RuntimeException(e);
         }
     }
 
-    @PutMapping(value = "/{appUserId/edit")
+    @PutMapping(value = "/{appUserId}/edit")
     public ResponseEntity<AppUserDTO> editAppUser(@RequestBody AppUserDTO appUserDTO, @PathVariable("appUserId") Long appUserId) {
         try {
-            AppUser editedUser = appUserServiceImp.update(appUserDTO);
+            AppUser editedUser = appUserService.update(appUserDTO);
             return ResponseEntity.ok(new AppUserDTO(editedUser));
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -53,7 +51,7 @@ public class AppUserController {
     @PostMapping(value = "/login")
     public ResponseEntity<AppUserDTO> login(@RequestBody UserLoginDTO userLoginDTO) {
         try {
-            AppUser user = appUserServiceImp.findByUsername(userLoginDTO.getUsername());
+            AppUser user = appUserService.findByUsername(userLoginDTO.getUsername());
             if (Objects.equals(user.getPassword(), userLoginDTO.getPassword())) {
                 return ResponseEntity.ok(new AppUserDTO(user));
             }
@@ -64,101 +62,103 @@ public class AppUserController {
     }
 
     @PutMapping("/watched")
-    public ResponseEntity<Void> setWatched(@RequestBody WatchedLessonDTO watchedLessonDTO){
-        try{
-            watchedListServiceImp.setWatched(watchedLessonDTO);
+    public ResponseEntity<Void> setWatched(@RequestBody WatchedLessonDTO watchedLessonDTO) {
+        try {
+            watchedListService.setWatched(watchedLessonDTO);
             return new ResponseEntity<>(HttpStatus.OK);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
     @PutMapping("/unwatched")
-    public ResponseEntity<Void> setUnwatched(@RequestBody WatchedLessonDTO watchedLessonDTO){
-        try{
-            watchedListServiceImp.setUnwatched(watchedLessonDTO);
+    public ResponseEntity<Void> setUnwatched(@RequestBody WatchedLessonDTO watchedLessonDTO) {
+        try {
+            watchedListService.setUnwatched(watchedLessonDTO);
             return new ResponseEntity<>(HttpStatus.OK);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
     @GetMapping("/{id}/courses")
     public ResponseEntity<AppUserCourseDTO> getUserCourses(@PathVariable Long id) {
-        try{
-            AppUser user = appUserServiceImp.findById(id);
+        try {
+            AppUser user = appUserService.findById(id);
             AppUserCourseDTO userCourseDTO = new AppUserCourseDTO();
             userCourseDTO.setMyCourses(user.getMyCourses());
             // VOLTAR AQUI - CURSOS ASSISTIDOS NÃO ESTÃO RETORNANDO
             return ResponseEntity.ok(userCourseDTO);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
 
     @PostMapping(value = "/addSuscribedCourses")
-    public ResponseEntity<AppUserCourseEditDTO> addSubscribedCourses(@RequestBody AppUserCourseEditDTO userDTO){
-        try{
-            appUserServiceImp.addSubscribedCourse(userDTO);
+    public ResponseEntity<AppUserCourseEditDTO> addSubscribedCourses(@RequestBody AppUserCourseEditDTO userDTO) {
+        try {
+            appUserService.addSubscribedCourse(userDTO);
             return ResponseEntity.ok(userDTO);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
+
     @DeleteMapping(value = "/removeSuscribedCourses")
-    public ResponseEntity<Void> removeSubscribedCourses(@RequestBody AppUserCourseEditDTO userDTO){
-        try{
-            appUserServiceImp.removeSubscribedCourse(userDTO);
+    public ResponseEntity<Void> removeSubscribedCourses(@RequestBody AppUserCourseEditDTO userDTO) {
+        try {
+            appUserService.removeSubscribedCourse(userDTO);
             return new ResponseEntity<>(HttpStatus.OK);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
     @PostMapping(value = "/addArchivedCourses")
-    public ResponseEntity<AppUserCourseEditDTO> addArchivedCourses(@RequestBody AppUserCourseEditDTO userDTO){
-        try{
-            appUserServiceImp.addArchivedCourse(userDTO);
+    public ResponseEntity<AppUserCourseEditDTO> addArchivedCourses(@RequestBody AppUserCourseEditDTO userDTO) {
+        try {
+            appUserService.addArchivedCourse(userDTO);
             return ResponseEntity.ok(userDTO);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
     @DeleteMapping(value = "/removeArchivedCourses")
-    public ResponseEntity<Void> removeArchivedCourses(@RequestBody AppUserCourseEditDTO userDTO){
-        try{
-            appUserServiceImp.removeArchivedCourse(userDTO);
+    public ResponseEntity<Void> removeArchivedCourses(@RequestBody AppUserCourseEditDTO userDTO) {
+        try {
+            appUserService.removeArchivedCourse(userDTO);
             return new ResponseEntity<>(HttpStatus.OK);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
     @PostMapping(value = "/addMyCourses")
-    public ResponseEntity<AppUserCourseEditDTO> addMyCourses(@RequestBody AppUserCourseEditDTO userDTO){
-        try{
-            appUserServiceImp.addMyCourse(userDTO);
+    public ResponseEntity<AppUserCourseEditDTO> addMyCourses(@RequestBody AppUserCourseEditDTO userDTO) {
+        try {
+            appUserService.addMyCourse(userDTO);
             return ResponseEntity.ok(userDTO);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
 
     @DeleteMapping(value = "/removeMyCourses")
-    public ResponseEntity<Void> removeMyCourses(@RequestBody AppUserCourseEditDTO userDTO){
-        try{
-            appUserServiceImp.removeMyCourse(userDTO);
+    public ResponseEntity<Void> removeMyCourses(@RequestBody AppUserCourseEditDTO userDTO) {
+        try {
+            appUserService.removeMyCourse(userDTO);
             return new ResponseEntity<>(HttpStatus.OK);
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
+
     @PutMapping(value = "/finishCourses")
     public ResponseEntity<AppUserCourseEditDTO> finishCourse(@RequestBody AppUserCourseEditDTO userDTO) {
         try {
-            appUserServiceImp.finishCourse(userDTO);
+            appUserService.finishCourse(userDTO);
             return ResponseEntity.ok(userDTO);
         } catch (RuntimeException e) {
             throw new RuntimeException(e.getMessage());
