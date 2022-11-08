7c92f2ce2a8e60921ae3ae1232d7f0959005e0c3
Caio André <caioserralvo182@gmail.com>
2022-11-08 19:14:01 -0300

    feat: LessonController / RoleController / SectionController

--- api/src/main/java/com/bookorange/api/controller/LessonController.java
+++ api/src/main/java/com/bookorange/api/controller/LessonController.java
@@ -1,4 +1,92 @@
 package com.bookorange.api.controller;
 
+import com.bookorange.api.domain.Lesson;
+import com.bookorange.api.dto.lessonDto.LessonCreateDTO;
+import com.bookorange.api.dto.lessonDto.LessonDTO;
+import com.bookorange.api.enumerator.ContentType;
+import com.bookorange.api.service.LessonService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lessons")
+@AllArgsConstructor
 public class LessonController {
+
+    private final LessonService lessonService;
+
+    @PostMapping(value = "/lessons")
+    public ResponseEntity<LessonDTO> createLesson(@RequestBody LessonCreateDTO lessonCreateDTO) {
+        try {
+            Lesson lessonCreated = lessonService.create(lessonCreateDTO);
+            return ResponseEntity.ok(new LessonDTO(lessonCreated));
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Lesson> findById(@RequestParam("lessonId") Long lessonId) {
+        try {
+            Lesson lesson = lessonService.findById(lessonId);
+            return ResponseEntity.ok(lesson);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<Lesson>> findAll() {
+        try {
+            List<Lesson> allLessons = lessonService.findAll();
+            return ResponseEntity.ok(allLessons);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(value = "/topic/{topic}")
+    public ResponseEntity<List<Lesson>> listByTopic(@PathVariable("topic") String topic) {
+        try {
+            List<Lesson> lessonsByTopic = lessonService.findByTopic(topic);
+            return ResponseEntity.ok(lessonsByTopic);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(value = "/content/{content}")
+    public ResponseEntity<List<Lesson>> listByContentType(@PathVariable("content") ContentType content) {
+        try {
+            List<Lesson> lessonsByContentType = lessonService.findByContentType(content);
+            return ResponseEntity.ok(lessonsByContentType);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PutMapping(value = "/update")
+    public ResponseEntity<Lesson> updateLesson(@RequestBody LessonDTO lessonDTO) {
+        try {
+            Lesson lessonUpdate = lessonService.update(lessonDTO);
+            return ResponseEntity.ok(lessonUpdate);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DeleteMapping(value = "/delete")
+    public ResponseEntity<Void> deleteLesson(@RequestParam("lessonId") Long lessonId) {
+        try {
+            lessonService.delete(lessonId);
+            return ResponseEntity.ok().build();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
--- api/src/main/java/com/bookorange/api/controller/RoleController.java
+++ api/src/main/java/com/bookorange/api/controller/RoleController.java
@@ -1,13 +1,10 @@
 package com.bookorange.api.controller;
 
-import com.bookorange.api.domain.AppUser;
 import com.bookorange.api.domain.Role;
-import com.bookorange.api.dto.appuserDto.AppUserDTO;
-import com.bookorange.api.dto.appuserDto.UserCreateDTO;
 import com.bookorange.api.dto.roleDto.RoleCreateDTO;
 import com.bookorange.api.dto.roleDto.RoleDTO;
-import com.bookorange.api.service.implementation.RoleServiceImp;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bookorange.api.service.RoleService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,15 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/roles")
+@AllArgsConstructor
 public class RoleController {
 
-    @Autowired
-    private RoleServiceImp roleServiceImp;
+    private final RoleService roleService;
 
     @PostMapping(value = "/create")
     public ResponseEntity<RoleDTO> createRole(@RequestBody RoleCreateDTO roleCreateDTO) {
         try {
-            Role createdRole = roleServiceImp.create(roleCreateDTO);
+            Role createdRole = roleService.create(roleCreateDTO);
             return ResponseEntity.ok(new RoleDTO(createdRole));
         } catch (RuntimeException e) {
             throw new RuntimeException(e);
