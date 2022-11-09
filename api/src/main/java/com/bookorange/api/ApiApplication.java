2303f2c07dffd9ce66b6f9f5443ce5b8bb79aa61
MarceloRibeiro1 <86325217+MarceloRibeiro1@users.noreply.github.com>
2022-11-08 21:57:08 -0300

    feat: swagger implementation

--- api/src/main/java/com/bookorange/api/ApiApplication.java
+++ api/src/main/java/com/bookorange/api/ApiApplication.java
@@ -2,12 +2,14 @@ package com.bookorange.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
