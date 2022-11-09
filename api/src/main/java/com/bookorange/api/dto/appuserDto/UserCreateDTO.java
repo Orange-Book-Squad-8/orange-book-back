080cec6a06ac70c79f63ee4c5c781290b342ea60
Caio André <caioserralvo182@gmail.com>
2022-11-09 20:55:49 -0300

    chore : adding new validation annotations in AppUserDTO

--- api/src/main/java/com/bookorange/api/dto/appuserDto/UserCreateDTO.java
+++ api/src/main/java/com/bookorange/api/dto/appuserDto/UserCreateDTO.java
@@ -4,19 +4,33 @@ import com.bookorange.api.enumerator.StackCategories;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class UserCreateDTO {
+
+    @Column(unique = true)
+    @NotBlank(message = "field not found")
+    @Length(min = 3, max = 30)
     private String username;
+
 
     private String password;
 
+    @Column(unique = true)
+    @NotBlank(message = "field not found")
+    @Email(message = "email not valid")
     private String email;
 
+    @NotNull
     private List<StackCategories> stackCategories;
 
     private String role;
