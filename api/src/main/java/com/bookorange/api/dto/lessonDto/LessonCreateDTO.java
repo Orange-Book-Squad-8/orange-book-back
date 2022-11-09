2c06973870a82586583848fbd67e8b7f055fd1f3
Caio André <caioserralvo182@gmail.com>
2022-11-09 20:57:22 -0300

    chore : adding new validation annotations in LessonDTO

--- api/src/main/java/com/bookorange/api/dto/lessonDto/LessonCreateDTO.java
+++ api/src/main/java/com/bookorange/api/dto/lessonDto/LessonCreateDTO.java
@@ -5,21 +5,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class LessonCreateDTO {
+
+    @NotBlank(message = "field not found")
     private String title;
 
+    @NotBlank(message = "field not found")
     private String description;
 
+    @NotBlank(message = "field not found")
     private String author;
 
+    @NotBlank(message = "field not found")
+    @Pattern(regexp = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)")
     private String link;
 
+    @NotBlank(message = "field not found")
     private String topic;
 
+    @NotNull
     private ContentType contentType;
 
+    @Positive
+    @Min(1)
     private Integer durationInMinutes;
 }
