69d00ae654881cd42add5db31d05fb973db2806e
Caio André <caioserralvo182@gmail.com>
2022-11-11 02:35:47 -0300

    fix bug

---
+++ api/src/main/java/com/bookorange/api/handler/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bookorange.api.handler.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
